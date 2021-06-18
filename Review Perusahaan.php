4e33d272f5686fa3eef19f67378014971e782c0c
rani822 <72178721+rani822@users.noreply.github.com>
2021-06-18 15:41:56 +0700

    Add files via upload

---
+++ Review Perusahaan.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+	<title>NEED.ID</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+</head>
+<body>
+
+
+
+<div class="content">
+	<header>
+		<h1 class="judul">NEED.ID</h1>
+		<h3 class="deskripsi"></h3>
+	</header>
+ 
+	<div class="menu">
+		<ul>
+			<li><a href="index.php?page=home">Lowongan Pekerjaan</a></li>
+			<li><a href="index.php?page=tentang">Review Perusahaan</a></li>
+			<li><a href="index.php?page=tutorial">Tips Karier</a></li>
+		</ul>
+	</div>
+
+<div class="halaman">
+	<h2>TELUSURI</h2>
+	<p>dan temukan jutaan review dan peringkat perusahaan terbaik saat ini!</p>
+
+	<form action="" method="get">
+	<label></label>
+	<input type="text" name="cari">
+	<input type="submit" value="Cari">
+
+
+	<br>
+
+	<h2>03 besar review dan peringkat perusahaan terbaik</h2>
+
+	<img width=200 height=260 src='gojek.png'>
+	<img width=200 height=260 src='indomaret.jpg'>
+	<img width=200 height=260 src='alfa.jpg'>
+
+</form>
+</div>
